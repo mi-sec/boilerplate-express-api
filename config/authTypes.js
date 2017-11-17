8b64f549aa989e2c18e864d9a6d12d27a721820c
iskore <iSkore@users.noreply.github.com>
2017-11-17 12:11:10 -0500

    Working on JWT authentication

---
+++ config/authTypes.js
@@ -0,0 +1,7 @@
+/** ****************************************************************************************************
+ * File: authTypes.js
+ * Project: boilerplate-express-api
+ * @author Nick Soggin <iSkore@users.noreply.github.com> on 17-Nov-2017
+ *******************************************************************************************************/
+'use strict';
+// @formatter:off
