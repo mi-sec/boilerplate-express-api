b1f2a1025ee3cebe0c5b377305f1ee2e032aca46
iskore <iSkore@users.noreply.github.com>
2017-11-20 11:34:29 -0500

    Working on ACL, JWT Token, etc.

---
+++ api/user/getPermissions.js
@@ -0,0 +1,7 @@
+/** ****************************************************************************************************
+ * File: getPermissions.js
+ * Project: boilerplate-express-api
+ * @author Nick Soggin <iSkore@users.noreply.github.com> on 20-Nov-2017
+ *******************************************************************************************************/
+'use strict';
+// @formatter:off
